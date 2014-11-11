bef505a2ef4ec8b10b441ec872b15f1d55e53c49
jobinbasani <jobinbasani@gmail.com>
2014-11-11 12:54:21 -0400

    Revert "Using grid layout"
    This reverts commit 97d386046279cb7dddb7abb711c91f89ad851629.

--- app/src/main/java/com/jobinbasani/numbertowords/MainActivity.java
+++ app/src/main/java/com/jobinbasani/numbertowords/MainActivity.java
@@ -8,15 +8,11 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewTreeObserver;
-import android.view.animation.AnimationUtils;
-import android.view.animation.GridLayoutAnimationController;
-import android.view.animation.LayoutAnimationController;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.TextView;
 
 import com.jobinbasani.numbertowords.adapters.NumberPanelAdapter;
-import com.jobinbasani.numbertowords.components.GridNumberCell;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -26,23 +22,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final GridLayout gridLayout = (GridLayout) findViewById(R.id.controlGrid);
-        final ViewTreeObserver observer = gridLayout.getViewTreeObserver();
-        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                System.out.println("from listener  " + gridLayout.getHeight());
-                System.out.println("from listener width " + gridLayout.getWidth());
-                addGridElements(gridLayout.getHeight(),gridLayout.getWidth());
-                gridLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-            }
-        });
-
-       // gridLayout.setLayoutAnimation(new LayoutAnimationController(AnimationUtils.loadAnimation(mContext,R.anim.alpha_anim)));
-
-        /*System.out.println(gridView.getHeight()+"****");
         final GridView gridView = (GridView) findViewById(R.id.controlGrid);
+
+        //addGridElements();
+        System.out.println(gridView.getHeight()+"****");
         final ViewTreeObserver observer = gridView.getViewTreeObserver();
         observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
@@ -51,19 +34,19 @@ public class MainActivity extends ActionBarActivity {
                 gridView.setAdapter(new NumberPanelAdapter(mContext,gridView.getHeight()));
                 gridView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
             }
-        });*/
+        });
     }
 
-    public void addGridElements(int height, int width){
+    /*public void addGridElements(){
         GridLayout gridControl = (GridLayout) findViewById(R.id.controlGrid);
         GridLayout.LayoutParams params = new GridLayout.LayoutParams();
         params.setGravity(Gravity.FILL);
         for(int i=0;i<16;i++){
-            GridNumberCell cell = new GridNumberCell(mContext,height,width);
-            cell.setText(i+"h");
-            gridControl.addView(cell);
+            TextView textView = new TextView(this);
+            textView.setText(i+" pos");
+            gridControl.addView(textView,i,params);
         }
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/jobinbasani/numbertowords/components/GridNumberCell.java
+++ app/src/main/java/com/jobinbasani/numbertowords/components/GridNumberCell.java
@@ -20,14 +20,4 @@ public class GridNumberCell extends TextView {
         setTextSize((float) (height * .08));
         setBackground(getResources().getDrawable(R.drawable.cell_border));
     }
-    public GridNumberCell(Context context, int height, int width) {
-        super(context);
-        setHeight(height/4);
-        setWidth(width/4);
-        setGravity(Gravity.CENTER);
-        setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-        setTextColor(getResources().getColor(android.support.v7.appcompat.R.color.abc_primary_text_material_dark));
-        setTextSize((float) ((height/4) * .09));
-        setBackground(getResources().getDrawable(R.drawable.cell_border));
-    }
 }
